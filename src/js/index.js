1e129adaeaabbce37da125e99b451dd9e88ec5cb
FernandoCR1976 <fernandocorralesm76@gmail.com>
2021-03-05 16:28:13 +0000

    inicio proyecto

--- src/js/index.js
+++ src/js/index.js
@@ -13,3 +13,17 @@ import { Home } from "./component/home.js";
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
+
+import "src/js/component/counter.css";
+//import "bootstrap/dist/css/bootstrap.css";
+import SecondsCounter from "src/js/component/SecondsCounter.js";
+
+let counter = 0;
+
+setInterval(function() {
+	ReactDOM.render(
+		<SecondsCounter seconds={counter} />,
+		document.getElementById("root")
+	);
+	counter += 1;
+}, 1000);
